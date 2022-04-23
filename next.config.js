dfc2478b3fa61e3d519fe56ed712e512524a76be
Luke Miller <lukelmiller@icloud.com>
2022-04-23 14:38:48 -0500

    Retry exports

--- next.config.js
+++ next.config.js
@@ -5,13 +5,7 @@ const nextConfig = {
   reactStrictMode: true,
   swcMinify: false,
   assetPrefix: process.env.NEXT_PUBLIC_BASE_PATH,
-  basePath: process.env.NEXT_PUBLIC_BASE_PATH
-}
-
-module.exports = nextConfig
-
-
-module.exports = {
+  basePath: process.env.NEXT_PUBLIC_BASE_PATH,
   webpack(config) {
     config.module.rules.push({
       test: /\.svg$/,
@@ -20,5 +14,19 @@ module.exports = {
 
     return config;
   }
-};
+}
+
+module.exports = nextConfig
+
+
+// module.exports = {
+//   webpack(config) {
+//     config.module.rules.push({
+//       test: /\.svg$/,
+//       use: ["@svgr/webpack"]
+//     });
+
+//     return config;
+//   }
+// };
 
