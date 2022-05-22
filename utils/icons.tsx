9cbcd2958d73a0a35e9608035fe886f147951aba
Luke Miller <lukelmiller@icloud.com>
2022-05-22 18:55:46 -0500

    Updated About Me Page
    feat: new gradient icons
    feat: about me, academics, and experience subsections
    todo: add copy and clean up layout

--- utils/icons.tsx
+++ utils/icons.tsx
@@ -1,13 +1,28 @@
 import CheckmarkIcon from "../assets/checkmark.svg";
 import GitHubIcon from "../assets/github.svg";
 import MailIcon from "../assets/mail.svg";
+import OliveGardenIcon from "../assets/olive-garden.svg";
+import PauseIcon from "../assets/pause.svg";
+import PlayIcon from "../assets/play.svg";
 import ProfileIcon from "../assets/profile.svg";
+import StarbucksIcon from "../assets/starbucks.svg";
 import SunIcon from "../assets/welcome.svg";
+import VisaIcon from "../assets/visa.svg";
+import MarketTechIcon from "../assets/market-tech.svg";
+import UarkIcon from "../assets/uark.svg";
+
 const icons = {
 	CHECKMARK: <CheckmarkIcon />,
 	GITHUB: <GitHubIcon />,
 	MAIL: <MailIcon />,
+	MARKET_TECH: <MarketTechIcon />,
+	OLIVE_GARDEN: <OliveGardenIcon />,
+	PAUSE: <PauseIcon />,
+	PLAY: <PlayIcon />,
 	PROFILE: <ProfileIcon />,
+	STARBUCKS: <StarbucksIcon />,
 	SUN: <SunIcon />,
+	UARK: <UarkIcon />,
+	VISA: <VisaIcon />,
 };
 export default icons;
