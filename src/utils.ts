dbcb4554c99c3f5979a310591be64550946a19af
Luke Miller <lukelmiller@icloud.com>
2024-09-29 14:02:19 -0500

    chore(logo): made it pretty

--- src/utils.ts
+++ src/utils.ts
@@ -1,3 +1,27 @@
+import autoModeIcon from "./assets/icons/hybrid-fill.svg?raw";
+import darkModeIcon from "./assets/icons/moon-fill.svg?raw";
+import lightModeIcon from "./assets/icons/sun-fill.svg?raw";
+
+export type Appearance = "auto" | "dark" | "light";
+
+export const autoModeContent = `${autoModeIcon}<span class="label">SYSTEM</span>`;
+export const darkModeContent = `${darkModeIcon}<span class="label">DARK</span>`;
+export const lightModeContent = `${lightModeIcon}<span class="label">LIGHT</span>`;
+
+export const appearanceContentMap = {
+	auto: autoModeContent,
+	dark: darkModeContent,
+	light: lightModeContent,
+};
+
+export const prefersDark = window.matchMedia(
+	"(prefers-color-scheme: dark)"
+).matches;
+
+export const appearanceOrder: Appearance[] = prefersDark
+	? ["auto", "light", "dark"]
+	: ["auto", "dark", "light"];
+
 export const getCurrentSeason = (date: Date) => {
 	const firstDayOfSpring = new Date(date.getFullYear(), 2, 22);
 	const firstDayOfSummer = new Date(date.getFullYear(), 6, 22);
@@ -22,4 +46,23 @@ export const setPreferences = (prefs: Record<string, unknown>) => {
 			...prefs,
 		})
 	);
+};
+
+export const initializeTheme = () => {
+	const date = new Date();
+	const currentSeason = getCurrentSeason(date);
+	const prefs = getPreferences();
+
+	const appearanceToggle = document.getElementById("appearance-toggle");
+	const styleRoot = document.getElementById("global-theme-root");
+	const themeSelect = document.getElementById("theme-select");
+
+	const appearance: Appearance = prefs.appearance || "auto";
+	const theme = prefs.theme || currentSeason;
+
+	if (styleRoot)
+		styleRoot.setAttribute("href", `/themes/${theme}-${appearance}.css`);
+	if (appearanceToggle)
+		appearanceToggle.innerHTML = appearanceContentMap[appearance];
+	if (themeSelect) (themeSelect as HTMLSelectElement).value = theme;
 };
